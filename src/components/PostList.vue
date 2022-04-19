ec588a1e5deb840731f70c20cddd092ea1731118
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-04-19 21:32:34 +0100

    All posts display section

---
+++ src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="post-lists">
+      <div v-for="post in posts" :key="post.id">
+        <SinglePost :post="post"/>
+        <!-- <h3>{{ post.title}}</h3> -->
+      </div>
+  </div>      
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+import SinglePost from './SinglePost.vue'
+export default{
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+        onMounted(() =>console.log('component mounted',props.posts))
+        onUnmounted(() =>console.log('component Unmounted',props.posts))
+        onUpdated(() =>console.log('component updated',props.posts))
+    }
+    
+
+}
+
+</script>
+
+<style>
+
+</style>
