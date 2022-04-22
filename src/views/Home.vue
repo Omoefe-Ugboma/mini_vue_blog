d8791c61b66dae5281ab017984a314c6e570ba99
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-04-22 20:53:55 +0100

    getPosts Composable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,32 +10,15 @@
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
 // @ is an alias to /src
-import PostList from "../components/PostList.vue";
-
+import PostList from "../components/PostList.vue"
+import getPosts from '../composables/getPosts'
 export default {
   name: "Home",
   components: { PostList },
   setup(){
-    const posts = ref([])
-    const error = ref(null)
 
-    const load = async () =>{
-      try{
-        let data = await fetch('http://localhost:3000/posts')
-        console.log(data)
-        if(!data.ok){
-          throw Error('no data available')
-        }
-        posts.value = await data.json()
-      }
- 
-      catch(err){
-        error.value =  err.message
-        console.log(error.value)
-      }
-    }
+   const { posts, error, load } = getPosts()
     load()
 
     return {posts, error}
